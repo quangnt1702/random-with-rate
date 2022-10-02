de8b659ac173036bb42e27f00659ad5ceb5bf225
QUANGNT\quang <quangnt1702@gmail.com>
2022-10-02 11:49:25 +0700

    init

---
+++ RandomWithRate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomWithRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
+            items.Add(new Item() { Name = "Anna", Rate = 10});
+            items.Add(new Item() { Name = "Alex", Rate = 20});
+            items.Add(new Item() { Name = "Dog", Rate = 150});
+            items.Add(new Item() { Name = "Cat", Rate = 30});
+
+            Item newItem = ProportionalWheelSelection.SelectItem(items);
+            Console.WriteLine(newItem.Name);
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Rate { get; set; }
+    }
+}
---
+++ RandomWithRate/ProportionalWheelSelection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomWithRate
+{
+    public class ProportionalWheelSelection
+    {
+        private static readonly Random Random = new Random();
+
+        public static Item SelectItem(List<Item> items)
+        {
+            int poolSize = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                poolSize += items[i].Rate;
+            }
+
+            int randomNumber = Random.Next(0, poolSize) + 1;
+
+            int accumulatedProbability = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                accumulatedProbability += items[i].Rate;
+                if (randomNumber <= accumulatedProbability)
+                {
+                    return items[i];
+                }
+            }
+
+            return null;
+        }
+    }
+}
